5b587d030a98127a0495486f1c82c1420c00448a
MitroiuBogdan <mitroiu.bogdan@gmail.com>
2020-10-17 12:28:33 +0300

    first commit

---
+++ src/main/java/com/yllu/springsecuritybasics/SpringSecurityBaseicsApplication.java
@@ -0,0 +1,13 @@
+package com.yllu.springsecuritybasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBaseicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBaseicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yllu/springsecuritybasics/SpringSecurityBaseicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yllu.springsecuritybasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBaseicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
