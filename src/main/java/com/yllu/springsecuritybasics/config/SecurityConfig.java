ab95d8c520d06ea335a4bd2f18a45830ac24cfab
MitroiuBogdan <mitroiu.bogdan@gmail.com>
2020-10-17 18:46:10 +0300

    clean setup

---
+++ src/main/java/com/yllu/springsecuritybasics/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.yllu.springsecuritybasics.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        var manager = new InMemoryUserDetailsManager();
+        var userDetails = User.withUsername("foo").password("foo").authorities("ADMIN").build();
+        manager.createUser(userDetails);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+
+    }
+
+
+}
